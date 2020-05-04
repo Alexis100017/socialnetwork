7d912bad10625de61a72d97880fb51e28ea36a10
AlexisIBr19 <alexiscalderon123@hotmail.com>
2020-05-04 11:20:55 -0500

    imported db config to connect the app to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
+const connectDB = require("./config/Db");
 const app = express();
+
+//connect db
+
+connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
 const PORT = process.env.PORT || 5000;
