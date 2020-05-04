85e3d30c34a07dcef952dde7aa81a622cea11411
AlexisIBr19 <alexiscalderon123@hotmail.com>
2020-05-04 11:21:40 -0500

    created default json where the URI config was set up

---
+++ config/Db.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+//mongoose.connect(db); //this will give us a promise, however there is a new standard for handling promises and is sync await
+//whenever its used a async await is good to do it in a try catch block
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log("Mongodb connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // exit process with failure
+  }
+};
+/*
+const connectDB = () => {
+  mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => {
+      console.log("Mongodb connected");
+    })
+    .catch((err) => {
+      console.error(err.message);
+      process.exit(1); // exit process with failure
+    });
+};
+*/
+module.exports = connectDB;
